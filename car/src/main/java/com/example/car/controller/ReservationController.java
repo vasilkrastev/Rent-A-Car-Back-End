cd176be9ac90ada34bffc686ba9507cd79673bef
Vasil Krastev <vasil_krastev@epam.com>
2021-06-14 01:52:27 +0300

    Hotfix

--- car/src/main/java/com/example/car/controller/ReservationController.java
+++ car/src/main/java/com/example/car/controller/ReservationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8090", allowedHeaders = "")
+@CrossOrigin(origins = "*")
 @RequestMapping("/reservation")
 public class ReservationController {
 
